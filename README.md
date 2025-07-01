d6a841d9c1e2cd78fdfa6b348dc069a4cab41a5e
mqzedd <153781300+mqzedd@users.noreply.github.com>
2025-07-01 17:31:54 +0930

    feat: add multithreading to api requests
    add multithreading to api requests
    add progress bar to api requests(might be redundant due to speed increase)
    reformat output

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # modcheck
-Checks minecraft mods by comparing SHA1 hashes
+Checks minecraft mods by comparing SHA1 hashes against modrinth
 
 To use:
 > Clone the repository and run main.py
-
 > Use the binary in release
+> Compile and create the binary using pyinstaller or auto-py-to-exe
+> Run the program while minecraft is open
+> ???
+> Profit
 
->Compile and create the binary using pyinstaller or auto-py-to-exe
+todo:
+Check deleted files
+Check edited files?
+Find javaw.exe program memory and run strings?!
--- main.py
+++ main.py
@@ -4,71 +4,74 @@ from os import listdir
 from os.path import isfile, join
 from json import loads
 import re
+from time import sleep
+from time import time
+from tqdm.contrib.concurrent import thread_map
 from directory import *
-
-# Python program to find the SHA-1 message digest of a file
 
 
 def hash_file(filename):
     """ "This function returns the SHA-1 hash
     of the file passed into it"""
-    # make a hash object
     h = hashlib.sha1()
-    # open file for reading in binary mode
     with open(filename, "rb") as file:
-        # loop till the end of the file
         chunk = 0
         while chunk != b"":
-            # read only 1024 bytes at a time
             chunk = file.read(1024)
             h.update(chunk)
-    # return the hex representation of digest
     return h.hexdigest()
 
 
 endpoint = "https://api.modrinth.com/v2/version_file/"
 
 
+def send_request(mod_hash):
+    r = requests.get(endpoint + str(mod_hash), params={"algorithm": "sha1"})
+    return r
+
+
 def modcheck(dir):
+    """Check the mods in the given directory against Modrinth's db on SHA-1 hashes"""
+
     main_dir = dir + "/mods"
     onlyfiles = [f for f in listdir(main_dir) if isfile(join(main_dir, f))]
-    mod_check = []
-    mod_renamed = []
+    file_hashes = [hash_file(main_dir + "/" + i) for i in onlyfiles]
+    api_results = []
+    """ for i in tqdm(file_hashes):
+        api_results.append(send_request(i))
+     """
+    api_results = thread_map(
+        send_request, file_hashes, max_workers=100, desc="Checking Hashes"
+    )  # uses threading to send the requests in parallel, appending the results to the api_results map
+    unverified_mods = []
+    mod_name_discrepancies = []
+    for index, i in enumerate(onlyfiles):
 
-    for i in onlyfiles:
         reg_file_name = re.sub(r" \(.*?\)", "", i)
-        if "CustomT" in reg_file_name:
-            print("yess")
-            print(i)
-            print(reg_file_name)
-        mod_hash = hash_file(main_dir + "/" + i)
-
-        r = requests.get(endpoint + str(mod_hash), params={"algorithm": "sha1"})
+        r = api_results[index]
         if r.status_code != 404:
             data = loads(r.text)
             modrinth_file_name = data["files"][0]["filename"]
-
             if modrinth_file_name != reg_file_name:
-                print(modrinth_file_name)
-                print(reg_file_name)
-                print(mod_hash)
-                mod_renamed.append(i)
+                mod_name_discrepancies.append(modrinth_file_name)
         else:
-            mod_check.append(i)
-
-    print(str(len(mod_check)) + " mods were found that did not appear on modrinth\n")
-    for i in mod_check:
-        print(i)
-
+            unverified_mods.append(i)
     print(
-        str(len(mod_renamed))
+        str(len(unverified_mods)) + " mods were found that did not appear on modrinth"
+    )
+    for i in unverified_mods:
+        print(i)
+    print(
+        str(len(mod_name_discrepancies))
         + " mods were found that did not match with modrinth names"
     )
-    for i in mod_renamed:
+    for i in mod_name_discrepancies:
         print(i)
 
 
+start = time()
 minecraft_directory = get_minecraft_working_directory()
+# minecraft_directory = "C:/Users/210284/Desktop/MC/MultiMC/instances/Simply Optimized-1.21-1.0(1)/.minecraft"
 if minecraft_directory:
     if type(minecraft_directory) != list:
         print("Minecraft working directory:", minecraft_directory)
@@ -76,11 +79,15 @@ if minecraft_directory:
     else:
         print("Multiple instances found.")
         for i in minecraft_directory:
-            print(i)
+            print("Minecraft working directory:", i + "\n")
+            sleep(0.5)
             modcheck(i)
 else:
     print("Minecraft instance not found.")
+end = time()
+print(f"Execution time: {end - start:.2f} seconds")
 input("Press Enter to continue...")
+
 
 """
 Find Minecraft Instances
